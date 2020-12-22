0c86fe9eb7ea946213946d833771664f7c89a8c7
Vladislav <vladby86@gmail.com>
2020-12-22 23:14:04 +0300

    Updates

---
+++ precache-manifest.888e92c54650710bb0db54015ceceb26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "302e40d321072d33ff0095eb181f4be6",
+    "url": "/Social_network_react_typescript/index.html"
+  },
+  {
+    "revision": "4c7b23d621b8aa08c241",
+    "url": "/Social_network_react_typescript/static/css/main.6a6f2eb9.chunk.css"
+  },
+  {
+    "revision": "5e1358506302f7c98076",
+    "url": "/Social_network_react_typescript/static/js/2.7b70b075.chunk.js"
+  },
+  {
+    "revision": "3e5bb88f3f2708c5dfec1e159b6a3a23",
+    "url": "/Social_network_react_typescript/static/js/2.7b70b075.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c7b23d621b8aa08c241",
+    "url": "/Social_network_react_typescript/static/js/main.d24f6201.chunk.js"
+  },
+  {
+    "revision": "47698124695ec823d9e0",
+    "url": "/Social_network_react_typescript/static/js/runtime-main.1c933c3f.js"
+  },
+  {
+    "revision": "d243bdd9c810d22b854c5d78b6c891ff",
+    "url": "/Social_network_react_typescript/static/media/logo.d243bdd9.png"
+  }
+]);
