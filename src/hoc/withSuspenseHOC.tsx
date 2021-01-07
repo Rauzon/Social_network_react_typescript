4e67eb8a5edf0d2c9b682e17d75984a5abdf5a1f
Vladislav <vladby86@gmail.com>
2021-01-08 00:58:27 +0300

    was fixed bug with showing circleProgress

--- src/hoc/withSuspenseHOC.tsx
+++ src/hoc/withSuspenseHOC.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import {CircularProgress} from "@material-ui/core";
+import s from "../app.module.css";
 
 
 export function withSuspenseHOC(Component: any) {
-    return <React.Suspense fallback={<CircularProgress/>}>
+    return <React.Suspense fallback={<CircularProgress className={s.circleProgress}/>}>
         < Component/>
     </React.Suspense>
 }
